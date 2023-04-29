395e0b5e7b4f7fc3882c9f21e4507b5d74224ce7
amirhfalakian <103897684+amirhfalakian@users.noreply.github.com>
2023-04-29 21:16:19 +0330

    Delete proxy.js

--- proxy.js
+++
@@ -1,9 +0,0 @@
-const cors_proxy = require('cors-anywhere');
-
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(process.env.PORT || 8080, () => {
-    console.log('Running CORS Anywhere on ' + (process.env.PORT || 8080));
-});
